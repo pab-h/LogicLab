aca19288c3f0bc073c4ff733e94dd021b5802dd5
pab-h <dev.pab.2020@gmail.com>
2024-03-28 22:13:43 -0300

    feat: helloFrame

--- logiclab/app.py
+++ logiclab/app.py
@@ -1,11 +1,24 @@
-class App(object):
+import tkinter as tk
+
+from logiclab.frames.helloFrame import HelloFrame
+
+class App(tk.Tk):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.title("LogicLab")
+        self.geometry("1280x720")
+        self.resizable(False, False)
+
+        self.helloFrame = HelloFrame(self)
+
     def __enter__(self):
-        print("App ligado")
+        print("Carregando todos os recursos....")
         return self
     
     def __exit__(self, exceptionType, exceptionValue, exceptionTraceback):
         print("App desligado")
 
     def run(self) -> None:
-        raise NotImplementedError()
+        self.mainloop()
     
